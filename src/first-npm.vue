d82f7f48c9a81c38d1b67e188ea3d6b3769a2ef4
506411223@qq.com <506411223@qq.com>
2018-02-12 14:15:39 +0800

    first commit

---
+++ src/first-npm.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <p>这是我的第一个上传到NPM的VUE组件</p>
+      <p>{{text}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name:'first',
+  props: {
+      text:'你好'
+  }
+}
+</script>
+<style>
+p{
+    color: red;
+}
+</style>
+
+
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import firstNpmCom from './first-npm.vue'
+
+const firstNpm = {
+  install: function (Vue) {
+    if (typeof window !== 'undefined' && window.Vue) {
+      Vue = window.Vue
+    }
+    Vue.component('firstNpm', firstNpmCom)
+  }
+}
+
+export default firstNpm
